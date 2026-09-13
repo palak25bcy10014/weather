04b30e29ddbd8d2a158591e59e8a0e07244fccd6
palakbhatla006 <palak.25bcy10014@vitbhopal.ac.in>
2026-09-14 00:26:18 +0530

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -1,13 +1,10 @@
 import tkinter as tk
 import requests
 
-API_KEY = "AIzaSyDVVSn-VPh8H2zdSlTPAb0BYkpNoBwR6sA"
-
 def get_weather():
     city = city_entry.get().strip()
 
     try:
-
         geo = requests.get(
             "https://geocoding-api.open-meteo.com/v1/search",
             params={"name": city, "count": 1}
@@ -17,40 +14,70 @@ def get_weather():
         lon = geo["results"][0]["longitude"]
 
         data = requests.get(
-            "https://weather.googleapis.com/v1/currentConditions:lookup",
+            "https://api.open-meteo.com/v1/forecast",
             params={
-                "key": API_KEY,
-                "location.latitude": lat,
-                "location.longitude": lon
+                "latitude": lat,
+                "longitude": lon,
+                "current": "temperature_2m,relative_humidity_2m,weather_code"
             }
         ).json()
 
-        temp = data["temperature"]["degrees"]
-        humidity = data["relativeHumidity"]
-        condition = data["weatherCondition"]["description"]["text"]
+        temp = data["current"]["temperature_2m"]
+        humidity = data["current"]["relative_humidity_2m"]
+        code = data["current"]["weather_code"]
+
+        weather_codes = {
+            0: "Clear Sky",
+            1: "Mainly Clear",
+            2: "Partly Cloudy",
+            3: "Cloudy",
+            45: "Fog",
+            51: "Drizzle",
+            61: "Rain",
+            71: "Snow",
+            95: "Thunderstorm"
+        }
+
+        weather = weather_codes.get(code, "Weather Condition")
 
         result.config(
             text=f"{city.title()}\n\n"
                  f"Temperature: {temp}°C\n"
                  f"Humidity: {humidity}%\n"
-                 f"Weather: {condition}"
+                 f"Weather: {weather}"
         )
 
     except:
-        result.config(text="City not found or API error")
+        result.config(text="City not found or network error")
+
 
 root = tk.Tk()
 root.title("Weather App")
 root.geometry("350x300")
 
-tk.Label(root, text="Weather App", font=("Times New Roman", 20)).pack(pady=20)
+tk.Label(
+    root,
+    text="Weather App",
+    font=("Times New Roman", 20, "bold")
+).pack(pady=20)
 
-city_entry = tk.Entry(root, font=("Times New Roman", 14))
+city_entry = tk.Entry(
+    root,
+    font=("Times New Roman", 14)
+)
 city_entry.pack(pady=10)
 
-tk.Button(root, text="Get Weather", command=get_weather).pack(pady=10)
+tk.Button(
+    root,
+    text="Get Weather",
+    font=("Times New Roman", 12),
+    command=get_weather
+).pack(pady=10)
 
-result = tk.Label(root, font=("Times New Roman", 13))
+result = tk.Label(
+    root,
+    font=("Times New Roman", 11)
+)
 result.pack(pady=20)
 
 root.mainloop()
